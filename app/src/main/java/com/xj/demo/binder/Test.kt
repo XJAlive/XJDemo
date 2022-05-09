b098545549665175e2f8b4abb8821b132ee7aaae
xiejian <xiejian@mama.cn>
2022-05-09 21:49:12 +0800

    单例

--- app/src/main/java/com/xj/demo/binder/Test.kt
+++ app/src/main/java/com/xj/demo/binder/Test.kt
@@ -1,11 +1,44 @@
 package com.xj.demo.binder
 
+import android.util.ArrayMap
+import android.util.LruCache
 import java.lang.reflect.InvocationHandler
 import java.lang.reflect.Method
 
 fun main() {
 //    testInline()
+//    val sparseArray = SparseArray<Any>()
+//    sparseArray.put(1, 2)
+//    val arrayMap = ArrayMap<Int, String>()
+//    for (i in 1..10) {
+//        arrayMap[i] = i.toString()
+//    }
+//    val arrayMap2 = ArrayMap<Int, String>()
+//    val hashSet = HashSet<Int>()
+//    val cache: LruCache<String, String>
 
+    val linkedHashMap: LinkedHashMap<String, String> = LinkedHashMap(16, 0.75f, true)
+    linkedHashMap.put("name1", "josan1")
+    linkedHashMap.put("name2", "josan2")
+    linkedHashMap.put("name3", "josan3")
+    println("开始时顺序：")
+    val iterator = linkedHashMap.entries.iterator()
+    while (iterator.hasNext()) {
+        val entry: Map.Entry<*, *> = iterator.next()
+        val key = entry.key as String
+        val value = entry.value as String
+        println("key:$key,value:$value")
+    }
+    println("通过get方法，导致key为name1对应的Entry到表尾")
+    linkedHashMap.get("name1")
+    val set2 = linkedHashMap.entries
+    val iterator2 = set2.iterator()
+    while (iterator2.hasNext()) {
+        val entry: Map.Entry<*, *> = iterator2.next()
+        val key = entry.key as String
+        val value = entry.value as String
+        println("key:$key,value:$value")
+    }
 }
 
 
