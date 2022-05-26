9461e1107a80e18acaaa855cfba216307e2ce7b6
xiejian <xiejian@mama.cn>
2022-05-26 18:43:49 +0800

    Rxjava

--- app/src/main/java/com/xj/demo/designmodel/Decorator.kt
+++ app/src/main/java/com/xj/demo/designmodel/Decorator.kt
@@ -3,17 +3,17 @@ package com.xj.demo.designmodel
 /**
  * 装饰器模式，在不改动原有类的基础上扩展类的功能
  */
-abstract class Person {
-    abstract fun dressUp();
+abstract class Aldult {
+    abstract fun dressUp()
 }
 
-class Man : Person() {
+class Man : Aldult() {
     override fun dressUp() {
         println("wear T-shirt")
     }
 }
 
-class Decorator(var person: Person) : Person() {
+class Decorator(var person: Aldult) : Aldult() {
     override fun dressUp() {
         wearHat()
         person.dressUp()
